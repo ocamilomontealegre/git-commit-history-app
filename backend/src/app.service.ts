c082ce22f0ba923eb7d2125d8626781a742795cb
Camilo Montealegre <ocamilomontealegre@gmail.com>
2023-11-19 14:34:49 -0500

    Set app.service.ts logic

--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -1,8 +1,26 @@
+/* eslint-disable prettier/prettier */
 import { Injectable } from '@nestjs/common';
+import axios from 'axios';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  async getCommits(username: string, repo: string): Promise<any> {
+    const url: string = `https://api.github.com/repos/${username}/${repo}/commits`;
+
+    try {
+      const response = await axios.get(url);
+
+      const commits = response.data.map((commit: any) => ({
+        sha: commit.sha,
+        message: commit.commit.message,
+        author: commit.commit.author.name,
+      }));
+
+      return commits;
+    } catch (error) {
+      // eslint-disable-next-line prettier/prettier
+      console.error('Error fetching commits:', error);
+      throw new Error('Failed to fetch commits.');
+    }
   }
 }
